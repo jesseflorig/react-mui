dfae6eedbc2c1193103a5fba1e6c3ded9aefe415
Jesse Florig <jesseflorig@gmail.com>
2017-07-07 11:54:35 -0400

    added redux

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import './App.css';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import PersonIcon from 'material-ui/svg-icons/social/person';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 import ClippyIcon from 'material-ui-community-icons/icons/clippy';
+import Cart from './components/cart/cartContainer';
 
 class App extends Component {
   render() {
@@ -23,10 +22,11 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h2>Welcome to React</h2>
           </div>
-          <p className="App-intro">
+          <div className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          </div>
           <RaisedButton label="Default" />
+          <Cart/>
         </div>
       </MuiThemeProvider>
     );
--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { Provider } from 'react-redux';
+import Store from './store';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
+const StoreInstance = Store();
 
 injectTapEventPlugin();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={StoreInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
